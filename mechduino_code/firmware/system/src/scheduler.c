ae90dc674aac950f28c473efb63112776dccac1e
Anondrzej <122182880+Anondrzej@users.noreply.github.com>
2025-11-20 11:50:35 +0100

    Debug scheduler

--- mechduino_code/firmware/system/src/scheduler.c
+++ mechduino_code/firmware/system/src/scheduler.c
@@ -17,7 +17,6 @@ typedef struct task_handle {
 
 typedef struct execution_window {
     TASK_HANDLE* task_list;             // List of tasks to execute
-    uint32_t number_of_tasks;
     uint32_t number;                    // Number of execution window
     struct execution_window* next;      // Pointer to another execution window
 } EXECUTION_WINDOW;
@@ -31,6 +30,8 @@ EXECUTION_WINDOW* execution_window_list;    // Contains all execution windows, i
 uint32_t next_free_task_id;                     
 uint32_t number_of_active_tasks;
 uint32_t active_execution_window;           // Number of currently running execution window
+
+bool scheduler_needs_init = true;
 
 
 /** Helper functions **/
@@ -51,39 +52,38 @@ TASK_HANDLE* get_task_by_id(uint32_t task_id) {
         return NULL;
     }
 
-    uint32_t i = 0;
+    if (execution_window_list == NULL) return NULL;
+
     EXECUTION_WINDOW* window = execution_window_list;
     TASK_HANDLE* task = execution_window_list->task_list;
-        
-    uint32_t offset = 0;
 
-    while (i < number_of_active_tasks) {
-        
+    while(window != NULL) {        
 
-        if ((i - offset) == window->number_of_tasks) {
-            window = window->next;
+        while(task != NULL) {
 
-            offset = i;
+            if (task->task_id == task_id) {
+                return task;
+            }
 
-            task = window->task_list;
-        } // Jump to another execution window
-
-        if (task->task_id == task_id) {
-            return task;
+            task = task->next_task;
         }
 
-        task = task->next_task;
+        window = window->next;
 
-        i++;
+        if (window == NULL) break;
+
+        task = window->task_list;
+    
     }
 
     return NULL;
+
 }
 
 /* Inserts task to execution window with number execution_offset higher than active_execution_window */
 void insert_task (TASK_HANDLE* task, uint32_t execution_offset) {
-    if (number_of_active_tasks == 0) { // No execution window existed
-        execution_window_list = (EXECUTION_WINDOW*) malloc(sizeof(EXECUTION_WINDOW*));
+    if (execution_window_list == NULL) { // No execution window existed
+        execution_window_list = (EXECUTION_WINDOW*) malloc(sizeof(EXECUTION_WINDOW));
         execution_window_list->number = execution_offset;
         execution_window_list->task_list = task;
         execution_window_list->next = NULL;
@@ -93,10 +93,10 @@ void insert_task (TASK_HANDLE* task, uint32_t execution_offset) {
     
     EXECUTION_WINDOW* window = execution_window_list;
 
-    while ((window->next->number < active_execution_window + execution_offset) && (window->next != NULL)) window = window->next;
+    while ((window->next != NULL) && (window->next->number < active_execution_window + execution_offset)) window = window->next;
 
-    if (window->next == NULL) { // Insert with new execution window at the end of execution_window_list
-        window->next = (EXECUTION_WINDOW*) malloc(sizeof(EXECUTION_WINDOW*));
+    if (window->next == NULL) { // Insert in new execution window at the end of execution_window_list
+        window->next = (EXECUTION_WINDOW*) malloc(sizeof(EXECUTION_WINDOW));
         window->next->task_list = task;
         window->next->number = active_execution_window + execution_offset;
         window->next->next = NULL;
@@ -105,20 +105,21 @@ void insert_task (TASK_HANDLE* task, uint32_t execution_offset) {
     else if (window->next->number == (active_execution_window + execution_offset)) { // Insert into already existing execution window
         TASK_HANDLE* insert = window->next->task_list;
 
-        if (insert->priority < task->priority) {
+        if (insert->priority <= task->priority) {
             task->next_task = insert;
-            window->next->task_list = insert;
+            window->next->task_list = task;
 
             return;
         }
-        while (insert->next_task->priority >= task->priority) insert = insert->next_task;
+
+        while ((insert->next_task != NULL) && (insert->next_task->priority <= task->priority)) insert = insert->next_task;
 
         task->next_task = insert->next_task;
         insert->next_task = task;
     }   
 
     else { // Insert with new execution window in between 2 existing ones
-        EXECUTION_WINDOW* new_window = (EXECUTION_WINDOW*) malloc(sizeof(EXECUTION_WINDOW*));
+        EXECUTION_WINDOW* new_window = (EXECUTION_WINDOW*) malloc(sizeof(EXECUTION_WINDOW));
         new_window->number = execution_offset;
         new_window->task_list = task;
         new_window->next = window->next;
@@ -132,22 +133,35 @@ void insert_task (TASK_HANDLE* task, uint32_t execution_offset) {
 
 void scheduler_init() {
     next_free_task_id = 1;
+
     number_of_active_tasks = 0;
+
     execution_window_list = NULL;
+
     active_execution_window = 0;
+
+    scheduler_needs_init = false;
 }
 
 void scheduler_run(void) {
     TIMER_ID_TYPE execution_window_timer = 0;
 
-    setup_timer(MILLISECOND_TIMER, execution_window_timer);
+    setup_timer(MILLISECOND_TIMER, &execution_window_timer);
 
     goto START;
 
     while (true) {
+
         if (reset_timer(execution_window_timer)) {
-            setup_timer(MILLISECOND_TIMER, execution_window_timer);
+            setup_timer(MILLISECOND_TIMER, &execution_window_timer);
         }
+
+        EXECUTION_WINDOW* old_window = execution_window_list;
+
+        if (old_window == NULL) goto END;
+        execution_window_list = execution_window_list->next;
+        
+        free(old_window);
 
         START:
 
@@ -155,7 +169,8 @@ void scheduler_run(void) {
 
         if (execution_window_list == NULL) goto END;
 
-        for (uint32_t i = 0;  i < execution_window_list->number_of_tasks; i++) {
+        while (execution_window_list->task_list != NULL) {
+
             TASK_HANDLE* active_task = execution_window_list->task_list;
 
             execution_window_list->task_list = active_task->next_task;
@@ -172,21 +187,26 @@ void scheduler_run(void) {
 
                 continue;
             }
+            else {
+                insert_task(active_task, active_task->execution_frequency);
+            }
 
-            insert_task(active_task, active_task->execution_frequency);
         }
 
         END:
 
-        TIMER_PRECISION timer_value = 0;
+        while(true) {
+            TIMER_PRECISION timer_value = 0;
 
-        while(timer_value < MINIMAL_EXECUTION_WINDOW_RUNTIME) {
-            get_timer(execution_window_timer, timer_value);
+            get_timer(execution_window_timer, &timer_value);
+
+            if (timer_value > MINIMAL_EXECUTION_WINDOW_RUNTIME) break;
+
         }
     }
 }
 
-SCHEDULER_ERROR add_task(void* task_callback, void* task_context, TASK_PRIORITY_LEVEL priority, uint32_t execution_frequency, uint32_t task_id) {
+SCHEDULER_ERROR add_task(void* task_callback, void* task_context, TASK_PRIORITY_LEVEL priority, uint32_t execution_frequency, uint32_t *task_id) {
     if (task_callback == NULL) return SCHEDULER_INVALID_CALLBACK;
 
     if (is_priority_invalid(priority)) return SCHEDULER_INVALID_PRIORITY;
@@ -203,7 +223,7 @@ SCHEDULER_ERROR add_task(void* task_callback, void* task_context, TASK_PRIORITY_
 
     insert_task(new_task, 1);
 
-    task_id = new_task->task_id;
+    *task_id = new_task->task_id;
     next_free_task_id++;
 
     number_of_active_tasks++;
@@ -212,38 +232,51 @@ SCHEDULER_ERROR add_task(void* task_callback, void* task_context, TASK_PRIORITY_
 }
 
 SCHEDULER_ERROR remove_task(uint32_t task_id) {
-    TASK_HANDLE* task = get_task_by_id(task_id);
+    TASK_HANDLE *task = get_task_by_id(task_id);
 
     if (task == NULL) return SCHEDULER_INVALID_ID;
 
-    EXECUTION_WINDOW* window = execution_window_list;
-    TASK_HANDLE* remove = execution_window_list->task_list;
+    EXECUTION_WINDOW *window = execution_window_list;
+    TASK_HANDLE *remove = execution_window_list->task_list;
+    bool is_remove_list_start = false;
 
-    do {
+    while (window != NULL) {
         if (remove == task) {
-            window->task_list = remove->next_task;
+            is_remove_list_start = true;
 
             goto END;
         }
-
-        for (uint32_t i = 0; i < window->number_of_tasks; i++) {
-            if (remove->next_task == task) {
-                remove->next_task = task->next_task;
-
-                goto END;
+        if (remove != NULL) {
+            while (remove->next_task != NULL) {
+                if (remove->next_task == task) {
+                    goto END;
+                }
+                remove = remove->next_task;
             }
-            remove = remove->next_task;
         }
 
         window = window->next;
         remove = window->task_list;
-    } while (window != NULL);
+    }
 
     return SCHEDULER_INVALID_ID;
 
     END:
+    if (window->number == active_execution_window) {
+        task->delete_flag = true;
 
-    free(remove);
+        return SCHEDULER_OK;
+    }
+
+    if (is_remove_list_start) {
+        window->task_list = task->next_task;
+        free(task);
+    }
+    else {
+        remove->next_task = task->next_task;
+
+        free(task);
+    }
 
     return SCHEDULER_OK;
 }
@@ -269,3 +302,4 @@ SCHEDULER_ERROR change_task_execution_frequency(uint32_t task_id, uint32_t execu
 
     return SCHEDULER_OK;
 }
+
